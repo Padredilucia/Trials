f9621cf175ab210dbe46229c0dc32d7952d79015
Howard <padredilucia@gmail.com>
2019-08-11 11:50:21 +0800

    confusing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const hbs = require('hbs');
+
 
 // configration of paths for Express
 const publicDirectorypath = path.join(__dirname, '../public');
