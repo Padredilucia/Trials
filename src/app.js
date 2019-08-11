b0975d7aaaf332b75b2f9c34412e37d1fd15fc8c
Howard <padredilucia@gmail.com>
2019-08-11 12:38:15 +0800

    Now it's working

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const hbs = require('hbs');
+console.log(req.body);
 
 // configration of paths for Express
 const publicDirectorypath = path.join(__dirname, '../public');
