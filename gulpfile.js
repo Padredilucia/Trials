e2789b877cbbac723c7092a2fa8e1b57f54e07c3
Howard <padredilucia@gmail.com>
2019-08-11 21:18:26 +0800

    add gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+
+gulp.task('default', function(){
+    console.log('hooray!');
+});
+
+gulp.task('html', function(){
+    console.log('html processed!');
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+});
