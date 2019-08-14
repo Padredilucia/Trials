c327a2f1aeb0a4179fc6127984ed35287fe96021
Howard <padredilucia@gmail.com>
2019-08-14 21:10:26 +0800

    corrected ejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
-const hbs = require('ejs');
+const ejs = require('ejs');
 const app = express();
 const port = process.env.PORT || 3000
 
